dd0bd7e4baffdacc7fed3cdb7ec482e781c8e3da
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2025-02-12 22:36:36 -0500

    feat(lib): Se crea inyeccion de dependencias, se implementa el patron repository, sea el modelo o entidad y se crea el splash

--- lib/app/presentation/modules/splash/views/splash_view.dart
+++ lib/app/presentation/modules/splash/views/splash_view.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+import '../../../../../main.dart';
+
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      debugPrint(
+        '💧💧💧💧💧💧💧💧 Ahora si se va a ejecutar el _init(), ya se ha pintado el widget SplashView',
+      );
+      _init();
+    });
+    debugPrint('Ya finalizo el initState 🔥🔥🔥🔥🔥🔥🔥🔥');
+  }
+
+  Future<void> _init() async {
+    final connectivityRepository = Injector.of(context).connectivityRepository;
+    final hasInternetConnection =
+        await connectivityRepository.hasInternetConnection;
+
+    if (hasInternetConnection) {
+      // Comprobamos si existe una sesión activa
+      debugPrint('✅✅✅✅✅ Tiene conexión a internet? $hasInternetConnection');
+    } else {
+      // Mostrar la vista de offline
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: 100,
+          height: 100,
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
 
+import 'app/data/repositories_implementation/authentication_repository_impl.dart';
+import 'app/data/repositories_implementation/connectivity_repository_impl.dart';
+import 'app/domain/repositories/authentication_repository.dart';
+import 'app/domain/repositories/connectivity_repository.dart';
 import 'app/my_app.dart';
 
 void main() {
   runApp(
-    const MyApp(),
+    Injector(
+      connectivityRepository: ConnectivityRepositoryImpl(),
+      authenticationRepository: AuthenticationRepositoryImpl(),
+      child: const MyApp(),
+    ),
   );
+}
+
+class Injector extends InheritedWidget {
+  const Injector({
+    super.key,
+    required super.child,
+    required this.connectivityRepository,
+    required this.authenticationRepository,
+  });
+
+  final ConnectivityRepository connectivityRepository;
+  final AuthenticationRepository authenticationRepository;
+
+  @override
+  // ignore: avoid_renaming_method_parameters
+  bool updateShouldNotify(_) => false;
+
+  static Injector of(BuildContext context) {
+    final injector = context.dependOnInheritedWidgetOfExactType<Injector>();
+    assert(injector != null, 'No se pudo obtener el Injector');
+
+    return injector!;
+  }
 }
