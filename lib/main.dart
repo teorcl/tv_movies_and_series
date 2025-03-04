9d0b5de6d7f280d9f816a4a59ecec9a1310ac136
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2025-03-04 08:01:03 -0500

    refactor(main): Se reemplaza el injector por un provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 import 'app/data/http/http.dart';
 import 'app/data/repositories_implementation/authentication_repository_impl.dart';
@@ -14,45 +15,24 @@ import 'app/my_app.dart';
 
 void main() {
   runApp(
-    Injector(
-      connectivityRepository: ConnectivityRepositoryImpl(
+    Provider<ConnectivityRepository>(
+      create: (context) => ConnectivityRepositoryImpl(
         Connectivity(),
         InternetChecker(),
       ),
-      authenticationRepository: AuthenticationRepositoryImpl(
-        const FlutterSecureStorage(),
-        AuthenticationApi(
-          Http(
-            client: http.Client(),
-            baseUrl: 'https://api.themoviedb.org/3',
-            apikey: '9bbf7db647d6cd1cd3ab5cc8ec23c9c1',
+      child: Provider<AuthenticationRepository>(
+        create: (context) => AuthenticationRepositoryImpl(
+          const FlutterSecureStorage(),
+          AuthenticationApi(
+            Http(
+              client: http.Client(),
+              baseUrl: 'https://api.themoviedb.org/3',
+              apikey: '9bbf7db647d6cd1cd3ab5cc8ec23c9c1',
+            ),
           ),
         ),
+        child: const MyApp(),
       ),
-      child: const MyApp(),
     ),
   );
-}
-
-class Injector extends InheritedWidget {
-  const Injector({
-    super.key,
-    required super.child,
-    required this.connectivityRepository,
-    required this.authenticationRepository,
-  });
-
-  final ConnectivityRepository connectivityRepository;
-  final AuthenticationRepository authenticationRepository;
-
-  @override
-  // ignore: avoid_renaming_method_parameters
-  bool updateShouldNotify(_) => false;
-
-  static Injector of(BuildContext context) {
-    final injector = context.dependOnInheritedWidgetOfExactType<Injector>();
-    assert(injector != null, 'No se pudo obtener el Injector');
-
-    return injector!;
-  }
 }
