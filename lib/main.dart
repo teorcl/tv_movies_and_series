8951bd5650c9fe5c8a684db5f13fcfece9e28d98
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2025-03-05 22:27:10 -0500

    feat(lib): Se crea repository y se usa multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -5,34 +5,43 @@ import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 
 import 'app/data/http/http.dart';
+import 'app/data/repositories_implementation/account_repository_impl.dart';
 import 'app/data/repositories_implementation/authentication_repository_impl.dart';
 import 'app/data/repositories_implementation/connectivity_repository_impl.dart';
 import 'app/data/services/remote/authentication_api.dart';
 import 'app/data/services/remote/internet_checker.dart';
+import 'app/domain/repositories/account_repository.dart';
 import 'app/domain/repositories/authentication_repository.dart';
 import 'app/domain/repositories/connectivity_repository.dart';
 import 'app/my_app.dart';
 
 void main() {
   runApp(
-    Provider<ConnectivityRepository>(
-      create: (context) => ConnectivityRepositoryImpl(
-        Connectivity(),
-        InternetChecker(),
-      ),
-      child: Provider<AuthenticationRepository>(
-        create: (context) => AuthenticationRepositoryImpl(
-          const FlutterSecureStorage(),
-          AuthenticationApi(
-            Http(
-              client: http.Client(),
-              baseUrl: 'https://api.themoviedb.org/3',
-              apikey: '9bbf7db647d6cd1cd3ab5cc8ec23c9c1',
+    MultiProvider(
+      providers: [
+        Provider<ConnectivityRepository>(
+          create: (_) => ConnectivityRepositoryImpl(
+            Connectivity(),
+            InternetChecker(),
+          ),
+        ),
+        Provider<AuthenticationRepository>(
+          create: (_) => AuthenticationRepositoryImpl(
+            const FlutterSecureStorage(),
+            AuthenticationApi(
+              Http(
+                client: http.Client(),
+                baseUrl: 'https://api.themoviedb.org/3',
+                apikey: '9bbf7db647d6cd1cd3ab5cc8ec23c9c1',
+              ),
             ),
           ),
         ),
-        child: const MyApp(),
-      ),
+        Provider<AccountRepository>(
+          create: (_) => AccountRepositoryImpl(),
+        ),
+      ],
+      child: const MyApp(),
     ),
   );
 }
